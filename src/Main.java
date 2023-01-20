a6e69a8291813bbb1e1f0cedff76fd52a3b87369
damridfwdfwefd <alexdamsem2004@gmail.com>
2023-01-20 21:06:55 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       Sample input 1; F = C * 1.8 + 32
+                Sample output 1:
+        91.22
+                Sample Input 2:
+        0
+                Sample output 91.33
+    }
+}
